2eab170a46e611e844046dbdc5676350f2e4f2f9
only-suo <52968608+only-suo@users.noreply.github.com>
2019-10-18 17:30:45 +0800

    add build

---
+++ build/precache-manifest.5acf7d54b25ac7b3f3545f5f04a37efa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae51d3dbe23b76a4f56a34ffd76214c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e7d7c30ad2b36f9737",
+    "url": "/static/css/2.6f57fa83.chunk.css"
+  },
+  {
+    "revision": "038a597f56f574ebe9e5",
+    "url": "/static/css/main.407b7862.chunk.css"
+  },
+  {
+    "revision": "58e7d7c30ad2b36f9737",
+    "url": "/static/js/2.6aedeb04.chunk.js"
+  },
+  {
+    "revision": "038a597f56f574ebe9e5",
+    "url": "/static/js/main.f0e9902b.chunk.js"
+  },
+  {
+    "revision": "55b1f72ad6cc545f0f0a",
+    "url": "/static/js/runtime-main.06b8b5ae.js"
+  },
+  {
+    "revision": "bda3c4d33bc3aa26b46aaa147dccfc28",
+    "url": "/static/media/bg.bda3c4d3.png"
+  },
+  {
+    "revision": "1cd3eda1005daf4f742760800338fc76",
+    "url": "/static/media/iconfont.1cd3eda1.svg"
+  },
+  {
+    "revision": "3360c264005210e016daa1695984550f",
+    "url": "/static/media/iconfont.3360c264.ttf"
+  },
+  {
+    "revision": "a51a8ac8f2cadb2048d4c6e80e44e88f",
+    "url": "/static/media/iconfont.a51a8ac8.woff"
+  },
+  {
+    "revision": "a644049f18b1fcf24fd643af7090d996",
+    "url": "/static/media/iconfont.a644049f.eot"
+  },
+  {
+    "revision": "7ad0ad8a2cb27235754eaa92299e9dd7",
+    "url": "/static/media/tou.7ad0ad8a.jpeg"
+  }
+]);
